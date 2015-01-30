f021e3ee8e910a74b40252f0928aff023945d30d
who03 <william.ho96@gmail.com>
2015-01-29 22:39:20 -0800

    Lab4 Weather commit

--- js/clock.js
+++ js/clock.js
@@ -10,3 +10,27 @@ function getTime() {
    var y = setTimeout(function(){getTime()}, 1000);
 }
 getTime();
+
+function getTempName(temp) {
+	if (temp < 60)
+		return "cold";
+	else if (temp < 70)
+		return "chilly";
+	else if (temp < 80)
+		return "nice";
+	else if (temp < 90)
+		return "warm";
+	else
+		return "hot";
+}
+
+function getTemp() {
+	$.getJSON("https://api.forecast.io/forecast/f73dc0abe8201ac16c940e6c7ce817ac/35.300399,-120.662362?callback=?",
+		function (data) {
+			$("#forcastLabel").html(data.daily.summary);
+			$("#forcastIcon").attr("src", "img/" + data.daily.icon + ".png");
+			$("body").addClass(getTempName(data.daily.temperatureMax));
+		});
+}
+
+window.onload = getTemp;
